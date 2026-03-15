65db318554c353e7798fc09c91ba44a42af36694
John Stowbunenko <144531085+TheLiter0@users.noreply.github.com>
2026-03-15 00:42:19 -0400

    NA

--- src/secrets.h
+++ src/secrets.h
@@ -1,5 +1,3 @@
 #pragma once
-#define WIFI_SSID "HotMessHousehold"
-#define WIFI_PASS "Patterson2022"
-#define WIFI_SSID2 "Nuevacasa"
-#define WIFI_PASS2 "BearSimbaElla1004"
+#define WIFI_SSID ""
+#define WIFI_PASS ""
